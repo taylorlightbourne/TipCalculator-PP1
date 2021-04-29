f17bbc7c1a1d9a3a4fb3f738ed8e5a4776a6f8dd
Taylor Lightbourne <79942688+taylorlightbourne@users.noreply.github.com>
2021-04-28 21:27:25 -0400

    functions work correctly

---
+++ index.js
@@ -0,0 +1,45 @@
+function newFunction(num) {
+    return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+function tipCalc(){
+const billCost = document.querySelector("#cost").value
+const billCostConverted = parseInt(billCost)
+const tipAmount = document.querySelector("#tip").value
+const tipAmountConverted = parseInt(tipAmount)
+const tipTotal = billCostConverted * (tipAmountConverted / 100)
+const amountPaid = billCostConverted + tipTotal
+const convertedAmountPaid = newFunction(amountPaid)
+
+const displayTip = document.querySelector(".tipAmount")
+displayTip.innerText = tipTotal
+
+const displayTotal = document.querySelector(".total")
+displayTotal.innerText = convertedAmountPaid
+
+const grabList = document.querySelector(".prev-bill")
+const listItem = document.createElement("li")
+listItem.innerText = amountPaid
+grabList.append(listItem)
+
+}
+
+const calcBtn = document.querySelector("#calculateBtn")
+function alertMe(){
+alert("ERROR!")
+}
+
+function changeButtonColor(){
+
+let color = Math.floor(Math.random() * 16777215).toString(16)
+console.log(color)
+
+calcBtn.style.backgroundColor = `#${color}`
+}
+
+calcBtn.addEventListener('click', function(e){
+tipCalc()
+changeButtonColor()
+})
+
+
